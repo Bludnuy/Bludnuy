7ce8d88bfe73c86eb2a447cf13901a697d1c1f71
Bludnuy <100961889+Bludnuy@users.noreply.github.com>
2022-06-25 22:48:24 +0300

    Почта на пульсе
    почта н апульсу

--- pulse/mailer/smart.php
+++ pulse/mailer/smart.php
@@ -13,13 +13,13 @@ $mail->CharSet = 'utf-8';
 $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = '';                 // Наш логин
-$mail->Password = '';                           // Наш пароль от ящика
+$mail->Username = 'junmaster087@gmail.com';                 // Наш логин
+$mail->Password = 'Bkmz158397';                           // Наш пароль от ящика
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465;                                    // TCP port to connect to
  
-$mail->setFrom('', 'Pulse');   // От кого письмо 
-$mail->addAddress('');     // Add a recipient
+$mail->setFrom('Bludnuy@yandex.ru', 'Pulse');   // От кого письмо 
+$mail->addAddress('Bludnuy@yandex.ru');     // Add a recipient
 //$mail->addAddress('ellen@example.com');               // Name is optional
 //$mail->addReplyTo('info@example.com', 'Information');
 //$mail->addCC('cc@example.com');
